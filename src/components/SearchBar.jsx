bf68ef96419947b385f948fe9b003617233d4214
KanizSanjila <kanizsm13@gmail.com>
2026-05-21 11:53:40 +0600

    added notfound and loading

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Search } from "lucide-react";
 import { Input } from "@heroui/react";
